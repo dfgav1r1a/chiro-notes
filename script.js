ea7867cdd0b713a2650ede6e561dcce28996fe21
dfgaviria <dfgaviria.dev@gmail.com>
2023-08-27 19:57:51 +0000

    finished entering all elements in the form, prepping for writing final css for layout

--- script.js
+++ script.js
@@ -2,4 +2,31 @@
 const date = document.querySelector('#date');
 date.valueAsDate = new Date();
 
+//for selection of items in sensory table
+const wnl = document.querySelectorAll('#wnl');
+const hypo = document.querySelectorAll('#hypo');
+const hyper = document.querySelectorAll('#hyper');
+
+wnl.forEach(option => {
+    option.addEventListener('click', ({target}) => {
+      if (target.style.border === 'none') target.style.border = 'solid 2px black'
+      else target.style.border = 'none'
+  })
+})
+
+hypo.forEach(option => {
+    option.addEventListener('click', ({target}) => {
+      if (target.style.border === 'none') target.style.border = 'solid 2px black'
+      else target.style.border = 'none'
+  })
+})
+
+hyper.forEach(option => {
+    option.addEventListener('click', ({target}) => {
+      if (target.style.border === 'none') target.style.border = 'solid 2px black'
+      else target.style.border = 'none'
+  })
+})
+
+
 
