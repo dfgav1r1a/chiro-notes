17ab0aac328d10931db308178c181db32a55e555
dfgaviria <dfgaviria.dev@gmail.com>
2023-09-06 20:55:48 +0000

    Adding selection functionality on the ctl table, refactoring event listener an prepping for drawing implementation

--- script.js
+++ script.js
@@ -2,31 +2,32 @@
 const date = document.querySelector('#date');
 date.valueAsDate = new Date();
 
+//Function to toggle the border on certain elements in the form
+const toggle = ({target}) => {
+  if (target.style.border === 'none') target.style.border = 'solid 1px black'
+  else target.style.border = 'none'
+}
+
 //for selection of items in sensory table
 const wnl = document.querySelectorAll('#wnl');
 const hypo = document.querySelectorAll('#hypo');
 const hyper = document.querySelectorAll('#hyper');
 
-wnl.forEach(option => {
-    option.addEventListener('click', ({target}) => {
-      if (target.style.border === 'none') target.style.border = 'solid 2px black'
-      else target.style.border = 'none'
-  })
-})
+wnl.forEach(option => option.addEventListener('click', toggle))
+hypo.forEach(option => option.addEventListener('click', toggle))
+hyper.forEach(option => option.addEventListener('click', toggle))
 
-hypo.forEach(option => {
-    option.addEventListener('click', ({target}) => {
-      if (target.style.border === 'none') target.style.border = 'solid 2px black'
-      else target.style.border = 'none'
+// for selection of items in the C T L table
+const ctlTable = document.querySelectorAll('#ctl tbody tr')
+//creating a copy of the original array to choose only the first 3 rows of the table
+const arr = Array.from(ctlTable)
+const ctlTableCopy = arr.slice(0, 3); 
+//doing an inner for loop inside the forEach because I don't want the first cells to be circled when clicked
+ctlTableCopy.forEach(element => {
+  for (let i = 1; i < element.cells.length; i ++) {
+      element.cells[i].addEventListener('click', toggle)
+    }
   })
-})
-
-hyper.forEach(option => {
-    option.addEventListener('click', ({target}) => {
-      if (target.style.border === 'none') target.style.border = 'solid 2px black'
-      else target.style.border = 'none'
-  })
-})
 
 
 
